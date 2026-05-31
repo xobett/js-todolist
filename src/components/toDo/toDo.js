d15d0ddee77eae4c27370220c529793322e9c2b4
Cesar Betancourt <cessarr862@gmail.com>
2026-05-30 22:12:27 -0600

    add: content layout progress

--- src/components/toDo/toDo.js
+++ src/components/toDo/toDo.js
@@ -1,6 +1,6 @@
-export {ToDo};
+import './toDo.css';
 
-class ToDo {
+export class ToDo {
     #id
     title;
     description;
--- src/controllers/uiController.js
+++ src/controllers/uiController.js
@@ -1,8 +1,10 @@
 import '../css/variables.css';
 import '../css/josh-comeau-reset.css';
 import '../css/xobett-reset.css';
+import '../css/components.css';
 import '../css/main.css';
 import '../css/navbar.css';
+import '../css/content.css';
 
 export { uiController };
 
